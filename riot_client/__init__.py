8b376cc6607fa1ec0418c49195a3ddefd5aa6095
Jhosevic Castañeda <zetsubou97@gmail.com>
2017-05-26 21:32:40 -0400

    Update __init__.py

--- riot_client/__init__.py
+++ riot_client/__init__.py
@@ -2,9 +2,7 @@ import json
 import urllib.parse
 import urllib.request
 
-#You can replace the 'na' with any region,
-#it'll be the same result
-BASE_URL = 'https://na.api.pvp.net/'
+BASE_URL = 'https://region.api.riotgames.com/'
 
 class Riot:
 	def __init__(self, riot_key=None):
@@ -12,22 +10,26 @@ class Riot:
 			raise TypeError("riot_key must be str")
 		self.riot_key = riot_key
 
-	def get(self, *args):
-		url = resolve(*args, key=self.riot_key)
+	def get(self, *args,region=None):
+		if region is None:
+			raise ValueError("You must pass a valid region")
+		url = resolve(region=region,*args, key=self.riot_key)
 		req = urllib.request.Request(url)
 		body = b''
 
-		resp = urllib.request.urlopen(req)
-		while True:
-			buf = resp.read()
-			if not buf:
-				break
-			body += buf
+		#resp = urllib.request.urlopen(req)
+		with urllib.request.urlopen(req) as resp:
+			while True:
+				buf = resp.read()
+				if not buf:
+					break
+				body += buf
 		return json.loads(body.decode())
 
-def resolve(*args, key=None):
+def resolve(*args, key=None,region=None):
 	args = [urllib.parse.quote(str(x)) for x in args]
-	url = urllib.parse.urljoin(BASE_URL, '/'.join(args))
+	temp = BASE_URL.replace('region',region)
+	url = urllib.parse.urljoin(temp, '/'.join(args))
 	if key != None:
 		url = url + '?api_key=' + key
 	return url
