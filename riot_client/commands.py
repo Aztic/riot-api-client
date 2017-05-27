836b21ebf7721a2a4363625894d9c8a58545dd22
Jhosevic Castañeda <zetsubou97@gmail.com>
2017-05-26 21:33:45 -0400

    Update commands.py

--- riot_client/commands.py
+++ riot_client/commands.py
@@ -1,56 +1,53 @@
 import os
-from riot_client import Riot
-RIOT_KEY = os.getenv('RIOT_API_KEY')
-client = Riot(RIOT_KEY)
-
 
 PLATFORM_ID = {'lan':'LA1', 'las':'LA2', 'br':'BR1','eune':'EUN1', 'euw' : 'EUW1', 'jp':'JP1','tr':'TR1', 'ru':'RU', 'oce':'OC1','na':'NA1', 'kr':'KR' }
 
 #Return a list contaning the free-week champions
-def free_week(a_client):
+def free_week(a_client,region=None):
+	if region is None:
+		region = PLATFORM_ID['na'].lower()
 	ids = []
 	champ = []
-	champions = a_client.get('api/lol/na/v1.2/champion')
+	champions = a_client.get('lol/platform/v3/champions',region=region)
 	try:
 		for i in champions['champions']:
 			if i['freeToPlay'] is True:
 				ids.append(i['id'])
 
 		for i in ids:
-			champ.append(a_client.get('api/lol/static-data/na/v1.2/champion/',i)['name'])
+			champ.append(a_client.get('lol/static-data/v3/champions/',i,region=region)['name'])
 		return champ
 	except:
 		return -1
 
-#Checks the mastery level with determined champion of any summoner
 def mastery_level(champ,region,summoner,a_client):
 	region = region.lower()
 	champ = champ.lower().title()
 	summoner = summoner.lower()
 	region_id = PLATFORM_ID[region]
 	try:
+		#client.get('api/lol',region,'v1.4/summoner/by-name',summoner)[summoner]
 		player_id = _summoner_by_name(summoner,region,a_client)['id']
-		champion_id = client.get('api/lol/static-data',region,'/v1.2/champion/')['data'][champ]['id']
-		returned_value = a_client.get('championmastery/location',region_id,'/player',player_id,'/champion',champion_id)
+		champion_id = a_client.get('lol/static-data/v3/champions/',region=PLATFORM_ID[region].lower())['data'][champ]['id']
+		returned_value = a_client.get('championmastery/location/',region_id,'/player/',player_id,'/champion/',champion_id,region=PLATFORM_ID[region].lower())
 		return returned_value
 	except:
 		return -1
 
-#Returns the tier and division of any summoner
 def get_division(region,summoner,a_client):
 	region = region.lower()
 	summoner = summoner.replace(" ", "").lower()
 	try:
 		sum_id = str(_summoner_by_name(summoner,region,a_client)['id'])
 		#dictionary of list
-		information = client.get('api/lol',region,'/v2.5/league/by-summoner',sum_id)[sum_id][0]
-		tier = information['tier'].lower().title()
+		information = client.get('lol/',region,'/v3/league/by-summoner/',sum_id,region=PLATFORM_ID[region].lower())[sum_id][0]
+		tier = information['tier'].capitalize()
 		division = information['entries'][0]['division']
 		return tier + ' ' + division
 	except:
 		return -1
-	
-#Returns the current league and name of all summoners in specific game
+
+
 def game_information(region,summoner,a_client):
 	ret_dict = {'100':{}, '200':{}}
 	game_participants = _current_game_participants(region,summoner,a_client)
@@ -67,14 +64,14 @@ def _summoner_by_name(summoner, region, a_client):
 	summoner = summoner.lower()
 	region = region.lower()
 	try:
-		return_info = client.get('api/lol',region,'v1.4/summoner/by-name',summoner)[summoner]
+		return_info = client.get('api/lol/',region,'/v3/summoner/by-name/',summoner,region=PLATFORM_ID[region].lower())[summoner]
 		return return_info
 	except:
 		return -1
 
-def _summoner_by_id(id, a_client):
+def _summoner_by_id(id, a_client,region):
 	try:
-		return_info = client.get('api/lol',region,'/v1.4/summoner',id)[id]
+		return_info = client.get('api/lol/v3/summoner/',id,region=PLATFORM_ID[region].lower())[id]
 		return return_info
 	except:
 		return -1
@@ -85,7 +82,8 @@ def _current_game_participants(region,summoner,a_client):
 	region_id = PLATFORM_ID[region]
 	try:
 		sum_id = str(_summoner_by_name(summoner,region,a_client)['id'])
-		return_info = a_client.get('observer-mode/rest/consumer/getSpectatorGameInfo',region_id,sum_id)['participants']
+		return_info = a_client.get('lol/spectator/v3/active-games/by-summoner/',sum_id,region=PLATFORM_ID[region].lower())['participants']
 		return return_info
 	except:
+		print("FAILED HERE!")
 		return -1
